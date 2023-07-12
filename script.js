f909ccda4951d8da6977b49ef72299a05cfb1b58
Emmanuel Cordero <emmanuelcordero@Seths-MacBook-Pro.local>
2023-07-12 18:36:39 -0400

    final-edit-highscore

--- script.js
+++ script.js
@@ -145,12 +145,42 @@ saveBtn.addEventListener("click", function (event) {
   renderMessage();
 });
 
+function displayTopScores() {
+  var topScores = JSON.parse(localStorage.getItem("TopScores"));
+  var top10List = document.getElementById("top10");
+  top10List.innerHTML = ""; // Clear the existing content of the list
+
+  // Loop through each score and create list items to display them
+  for (var i = 0; i < topScores.length; i++) {
+    var scoreItem = document.createElement("li");
+    scoreItem.textContent = topScores[i].typedInitials + " - " + topScores[i].currentTime;
+    top10List.appendChild(scoreItem);
+  }
+}
+
+// Retrieve existing top scores from local storage
 function renderMessage() {
   var allScores = JSON.parse(localStorage.getItem("InitialAndNum"));
   if (allScores !== null) {
     document.querySelector(".message").textContent = "You " + allScores.typedInitials +
       " received a time of " + allScores.currentTime + ".";
   }
+  // Add the current score to the top scores list
+  // Sort the top scores list based on the currentTime property in descending order
+  var topScores = JSON.parse(localStorage.getItem("TopScores")) || [];
+  topScores.push(allScores);
+  topScores.sort(function (a, b) {
+    return b.currentTime - a.currentTime;
+  });
+
+  // Keep only the top 10 scores
+  topScores = topScores.slice(0, 10);
+
+  // Store the updated top scores list back to local storage
+  localStorage.setItem("TopScores", JSON.stringify(topScores));
+
+  // Display the top scores in the top10 element
+  displayTopScores();
 }
 
 // Click Reset to Reset
@@ -166,7 +196,8 @@ function resetQuiz() {
 
 // Calls local storage from web browser
 function init() {
-renderMessage();
+  renderMessage();
+  displayTopScores();
 }
 
 // Calls local storage when browser opens
