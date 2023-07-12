f9cba07f84c9a4260e05cd7338c37e276e20ba4d
Emmanuel Cordero <emmanuelcordero@Seths-MacBook-Pro.local>
2023-07-12 16:38:52 -0400

    added local storage

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 // Variables for the reset button
 var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var first = document.querySelector(".first");
-var firstinitials = document.querySelector(".firstinitials");
-var second = document.querySelector(".second");
-var secondsinitials = document.querySelector(".secondsinitials");
-var third = document.querySelector(".third");
-var thirdinitials = document.querySelector(".thirdinitials");
-
+var initialBox = document.querySelector("#high-score");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 var resetButton = document.querySelector(".reset-button");
+var timer;
+var timerCount;
 
-// getting quiz container
-var quizContainer = document.querySelector(".quiz-container")
+// Getting quiz container
+var quizContainer = document.querySelector(".quiz-container");
 
-var answerCorrect = 1;
-var answerWrong = 0;
+// This makes the "quiz is over" pop up in the time box
+var answerSum = document.querySelector(".timer-count");
 
-//This makes the "quiz is over" pop up in the time box
-var answersum = document.querySelector(".timer-count");
+var scoreCounter = 0;
 
-var winCounter = 0;
-var loseCounter = 0;
-var firstPlace = 0;
-var firstInitials = "";
-var secondPlace = 0;
-var secondPlaceInitials = "";
-var thirdPlace = 0;
-var thirdPlaceInitials = "";
-var isWin = false;
-
+var correctAnswers = ["head", "2 Spaces", "can only be true or false", "===", "cd", "Flex-Wrap"];
 var quizQuestions = [
   {
     question: "Which of the following tags contains metadata about the webpage?",
@@ -41,310 +26,147 @@ var quizQuestions = [
     question: "According to the W3C recommendation, what's the appropriate way of indenting nested HTML elements?",
     choices: ["No recommended indent", "No Space", "1 Space", "2 Spaces"],
     correct: "2 Spaces"
-  }
+  },
   {
-    question: "What comparison means the same value and same type?,
-    choices: ["No recommended indent", "No Space", "1 Space", "2 Spaces"],
-    correct: "2 Spaces"
-  }
+    question: "What is true about boolean data types?",
+    choices: ["is Undefined", "can only be a string", "can only be a number", "can only be true or false"],
+    correct: "can only be true or false"
+  },
   {
-    question: "xample Question: According to the W3C recommendation, what's the appropriate way of indenting nested HTML elements?",
-    choices: ["No recommended indent", "No Space", "1 Space", "2 Spaces"],
-    correct: "2 Spaces"
-  }
-  {
-    question: " What comparison means the same value and same type?",
-    choices: ["No recommended indent", "No Space", "1 Space", "2 Spaces"],
-    correct: "2 Spaces"
-  }
-  {
-    question: "xample Question: According to the W3C recommendation, what's the appropriate way of indenting nested HTML elements?",
+    question: "What comparison means the same value and same type?",
     choices: ["&&", "==", "===", "||"],
     correct: "==="
-  }
+  },
   {
-    question: "What command is to open a directory?",
+    question: "What command is used to open a directory?",
     choices: ["touch", "mkr", "git", "cd"],
     correct: "cd"
-  }
+  },
   {
-    question: " What flex style tool allows elemets to wrap around each other?",
+    question: "What flex style tool allows elements to wrap around each other?",
     choices: ["Flex-Wrap", "Flex-Direction", "Justify-content", "Display"],
     correct: "Flex-Wrap"
   }
-]
+];
+
+var questionsIndex = 0;
 
 // Click timer to start
 startButton.addEventListener("click", startGame);
 
 // The startGame function is called when the start button is clicked
 function startGame() {
-  isWin = false;
+  // isWin = false;
   timerCount = 60;
-    // Prevents start button from being clicked when round is in progress
+  // Prevents start button from being clicked when round is in progress
   startButton.disabled = true;
-    // renderBlanks()
+  // renderBlanks()
   startTimer();
-  renderQuestions()
+  renderQuestions();
+  initialBox.classList.add("hide");
 }
-
-var questionsindex = 0
 
 function renderQuestions() {
-  quizContainer.textContent = ""
-  // check to see if the question index is greater than the length of question -> return
+  quizContainer.textContent = "";
+  // Check to see if the question index is greater than the length of questions -> return
+  if (questionsIndex >= quizQuestions.length) {
+    quizComplete();
+    return;
+  }
+  var question = document.createElement("h2");
+  question.textContent = quizQuestions[questionsIndex].question;
+  quizContainer.append(question);
 
-  var question = document.createElement('h2')
-  question.textContent = quizQuestions[questionsindex].question
-  quizContainer.append(question)
+  var options = document.createElement("ul");
+  quizContainer.append(options);
 
-  var options = document.createElement('ul')
-  quizContainer.append(options)
+  for (var i = 0; i < quizQuestions[questionsIndex].choices.length; i++) {
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = quizQuestions[questionsIndex].choices[i];
+    options.append(choiceBtn);
 
-  for(var i = 0; i < quizQuestions[questionsindex].choices.length; i++) {
-    var choiceBtn = document.createElement('button')
-    choiceBtn.textContent = quizQuestions[questionsindex].choices[i]
-    options.append(choiceBtn)
-
-    choiceBtn.addEventListener("click", function(event) {
-        var chosen = event.target.textContent
-        if (chosen === quizQuestions[questionsindex].correct) {
-          console.log("correct")
-        } else {
-          console.log("incorrect")
-        }
-        questionsindex++
-        renderQuestions()
-    })
-  } 
-
+    choiceBtn.addEventListener("click", function (event) {
+      var chosen = event.target.textContent;
+      if (chosen === quizQuestions[questionsIndex].correct) {
+        scoreCounter++;
+        console.log("correct");
+      } else {
+        timerCount -= 5;
+        console.log("incorrect");
+      }
+      questionsIndex++;
+      console.log(chosen);
+      renderQuestions();
+    });
+  }
 }
 
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-var timer;
-var timerCount;
+function quizComplete() {
+  // Add remaining seconds to the score
+  scoreCounter += timerCount;
+
+  // Show form to retrieve user initials
+  // Make initial-box un-invisible
+  initialBox.classList.remove("hide");
+  // Stop timer
+  clearInterval(timer);
+}
 
 function startTimer() {
   timer = setInterval(function () {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      if (isWin && timerCount > 0) {
-        clearInterval(timer);
-        winGame();
-      }
-    }
-  // Clears interval and stops timer
-    if (timerCount === 0) {
-      clearInterval(timer);
-      loseGame();
+    if (timerCount <= 0) {
+      quizComplete();
+    } else {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      localStorage.setItem("timerCount", timerCount);
     }
   }, 1000);
 }
 
+var saveBtn = document.querySelector(".submit");
+saveBtn.addEventListener("click", function (event) {
+  event.preventDefault();
 
-//Error doesn't stop
-//This funtions makes the reset work,
-//This Function allows the local memory to stay on the site.
-// !!Need to Make Code to add up answers!! The quizselect function is called when you choose an asnwer button on a quiz
-// The quizselect function is called when you choose an answer button on a quiz
-// function quizselect() {
-//   isWin = false;
-//   timerCount = 60;
-// // Prevents start button from being clicked when round is in progress
-//   startButton.disabled = true;
-//   startTimer();
-// }
+  // Save Initials
+  var currentTime = localStorage.getItem("timerCount");
+  var typedInitials = document.getElementById("initials");
 
-// Click answers on quizes
-// startButton.addEventListener("click", quizselect);
+  // var currentTime = localStorage.getItem("timerCount", timerCount);
+  // var typedInitials = document.getElementById("initials");
 
-// function answerselect() {
-// // !!Need to Make Code for button select and to add up answers!! If less than 5 fail quiz
-// answerCorrect = 1;
-// answerWrong = 0;
-// }
+  var combinedInitialAndNum = {
+    currentTime: currentTime,
+    typedInitials: typedInitials.value.trim()
+  };
 
-
-
-
-// Click answers on quizzes
-var answerButtons = document.querySelectorAll(".answer-button");
-answerButtons.forEach(function (button) {
-  button.addEventListener("click", quizselect);
+  localStorage.setItem("InitialAndNum", JSON.stringify(combinedInitialAndNum));
+  renderMessage();
 });
 
-// !!answersum var maybe wrong!! The winGame function is called when the win condition is met
-function winGame() {
-  answersum.textContent = "YOU BEAT THE QUIZ!";
-  winCounter++;
-  startButton.disabled = false;
-  setWins();
-}
-
-// !!answersum var maybe wrong!! The loseGame function is called when the timer reaches 0
-function loseGame() {
-  answersum.textContent = "QUIZ OVER";
-  loseCounter++;
-  startButton.disabled = false;
-  setLosses();
+function renderMessage() {
+  var allScores = JSON.parse(localStorage.getItem("InitialAndNum"));
+  if (allScores !== null) {
+    document.querySelector(".message").textContent = "You " + allScores.typedInitials +
+      " received a time of " + allScores.currentTime + ".";
+  }
 }
 
 // Click Reset to Reset
 resetButton.addEventListener("click", resetQuiz);
 
 function resetQuiz() {
-    // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  firstPlace = 0;
-  firstInitials = 0;
-  secondPlace = 0;
-  secondPlaceInitials = 0;
-  thirdPlace = 0;
-  thirdPlaceInitials = 0;
-
-  // Renders (Wins, Losses, First, Second, third) and sets them into client storage
-  setWins();
-  setLosses();
-  setFirst();
-  setFirstInitials();
-  setSecond();
-  setSecondInitials();
-  setThird();
-  setThirdInitials();
+  scoreCounter = 0;
+  questionsIndex = 0;
+  timerElement.textContent = "";
+  startButton.disabled = false;
+  initialBox.classList.add("hide");
 }
 
-// Functions to update counts on the screen and in local storage
-//wins
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
+// Calls local storage from web browser
+function init() {
+renderMessage();
 }
-//losses
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-//First place time
-function setFirst() {
-  first.textContent = firstPlace;
-  localStorage.setItem("firstPlace", firstPlace);
-}
-//First place initials
-function setFirstInitials() {
-  firstinitials.textContent = firstInitials;
-  localStorage.setItem("firstInitials", firstInitials);
-}
-//Second place time
-function setSecond() {
-  second.textContent = secondPlace;
-  localStorage.setItem("secondPlace", secondPlace);
-}
-//Second place initials
-function setSecondInitials() {
-  secondsinitials.textContent = secondPlaceInitials;
-  localStorage.setItem("secondPlaceInitials", secondPlaceInitials);
-}
-//Third place time
-function setThird() {
-  third.textContent = thirdPlace;
-  localStorage.setItem("thirdPlace", thirdPlace);
-}
-//Third place initials
-function setThirdInitials() {
-  thirdinitials.textContent = thirdPlaceInitials;
-  localStorage.setItem("thirdPlaceInitials", thirdPlaceInitials);
-}
-
-
-// // // Calls local storage from web browser
-// function init() {
-//   getWins();
-//   getlosses();
-//   getfirstplacetime();
-//   getfirstplaceintitial();
-//   getsecondplacetime();
-//   getsecondplaceinitial();
-//   getthirdplacetime();
-//   getthirdplaceinitial();
-// }
 
 // Calls local storage when browser opens
 init();
-
-// Function to initialize the counts and initials from local storage
-function init() {
-  var storedWin = localStorage.getItem("winCount");
-  if (storedWin) {
-    winCounter = parseInt(storedWin);
-    win.textContent = winCounter;
-  }
-
-  var storedLose = localStorage.getItem("loseCount");
-  if (storedLose) {
-    loseCounter = parseInt(storedLose);
-    lose.textContent = loseCounter;
-  }
-
-  var storedFirst = localStorage.getItem("firstPlace");
-  if (storedFirst) {
-    firstPlace = parseInt(storedFirst);
-    first.textContent = firstPlace;
-  }
-
-  var storedFirstInitials = localStorage.getItem("firstInitials");
-  if (storedFirstInitials) {
-    firstInitials = parseInt(storedFirstInitials);
-    firstinitials.textContent = firstInitials;
-  }
-
-  var storedSecond = localStorage.getItem("secondPlace");
-  if (storedSecond) {
-    secondPlace = parseInt(storedSecond);
-    second.textContent = secondPlace;
-  }
-
-  var storedSecondInitials = localStorage.getItem("secondPlaceInitials");
-  if (storedSecondInitials) {
-    secondPlaceInitials = parseInt(storedSecondInitials);
-    secondsinitials.textContent = secondPlaceInitials;
-  }
-
-  var storedThird = localStorage.getItem("thirdPlace");
-  if (storedThird) {
-    thirdPlace = parseInt(storedThird);
-    third.textContent = thirdPlace;
-  }
-
-  var storedThirdInitials = localStorage.getItem("thirdPlaceInitials");
-  if (storedThirdInitials) {
-    thirdPlaceInitials = parseInt(storedThirdInitials);
-    thirdinitials.textContent = thirdPlaceInitials;
-  }
-}
-
-// // These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
